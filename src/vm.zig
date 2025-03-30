4069707c724cddb33cbab5a1232695eb38ccb0af
atemmel <temmeladam@gmail.com>
2025-03-30 16:52:54 +0200

    Began work on VM

--- src/vm.zig
+++ src/vm.zig
@@ -1,21 +1,58 @@
 const std = @import("std");
 const print = std.debug.print;
 
-pub const Op = enum {
-    Constant,
+pub const Op = union(enum) {
+    Constant: usize,
     Return,
 };
 
-pub const Chunk = std.ArrayList(Op);
+pub const Instructions = std.ArrayList(Op);
 
-pub const Value = struct {};
+pub const Chunk = struct {
+    instructions: Instructions,
+    constants: Values,
+
+    pub fn deinit(self: *Chunk) void {
+        self.instructions.deinit();
+        self.constants.deinit();
+    }
+
+    pub fn addInstruction(self: *Chunk, op: Op) !void {
+        return try self.instructions.append(op);
+    }
+
+    pub fn addConstant(self: *Chunk, value: Value) !usize {
+        try self.constants.append(value);
+        return self.constants.items.len - 1;
+    }
+};
+
+pub const Value = struct {
+    as: union {
+        integer: i64,
+    },
+
+    pub fn format(
+        self: Value,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        switch (self.as) {
+            .integer => |i| try writer.print("{}", .{i}),
+        }
+    }
+};
 
 pub const Values = std.ArrayList(Value);
 
 pub fn disassembleChunk(chunk: *Chunk, name: []const u8) void {
     print("== {s} ==\n", .{name});
     var offset: usize = 0;
-    while (offset < chunk.items.len) {
+    while (offset < chunk.instructions.items.len) {
         offset = disassembleInstruction(chunk, offset);
     }
 }
@@ -23,10 +60,11 @@ pub fn disassembleChunk(chunk: *Chunk, name: []const u8) void {
 fn disassembleInstruction(chunk: *Chunk, offset: usize) usize {
     print("{:0>4} ", .{offset});
 
-    const inst = chunk.items[offset];
+    const inst = chunk.instructions.items[offset];
 
     return switch (inst) {
         .Return => simpleInstruction(inst, offset),
+        .Constant => constantInstruction(inst, offset),
     };
 }
 
@@ -35,12 +73,22 @@ fn simpleInstruction(op: Op, offset: usize) usize {
     return offset + 1;
 }
 
+fn constantInstruction(op: Op, offset: usize) usize {
+    _ = op; // autofix
+    _ = offset; // autofix
+    return 0;
+}
+
 test "vm" {
-    var chunk = Chunk.init(std.testing.allocator);
+    var chunk: Chunk = .{
+        .instructions = Instructions.init(std.testing.allocator),
+        .constants = Values.init(std.testing.allocator),
+    };
     defer chunk.deinit();
 
-    try chunk.append(.Return);
-    try chunk.append(.Return);
+    const constant = try chunk.addConstant(.{ .as = .{ .integer = 5 } });
+    try chunk.addInstruction(Op{ .Constant = constant });
+    try chunk.addInstruction(.Return);
 
     disassembleChunk(&chunk, "example_chunk");
 }
